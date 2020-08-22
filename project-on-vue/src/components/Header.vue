8b67619fed96e0b84b50026af3a6a7da0f227b8c
YuriiOL <67682937+YuriiOL@users.noreply.github.com>
2020-08-22 14:16:00 +0300

    Creat project vue js

---
+++ project-on-vue/src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="header">
+    <div>1 Изображение</div>
+    <div>2 Рама</div>
+    <div>3 Заголовок</div>
+    <div>4 Цвет Отпечатков</div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
---
+++ project-on-vue/src/views/Wrapper.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="wrapper">
+    <Header></Header>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Header from "@/components/Header.vue";
+export default {
+  name: "Wrapper",
+  components: { Header }
+};
+</script>
